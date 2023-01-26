91ee53937284c8f2442dc21b4d12cb7329ef3d0e
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-01-26 15:30:11 +0100

    K2: Adjust original vs. smartcast conflict resolver for invokes
    See testData/diagnostics/tests/resolve/invoke/kt9517.kt
    It starts failing after the previous commit because we stop filtering out
    overrides there and it was necessary for kt9517.kt

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/FilteringOutOriginalInPresenceOfSmartCastConeCallConflictResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/FilteringOutOriginalInPresenceOfSmartCastConeCallConflictResolver.kt
@@ -5,6 +5,8 @@
 
 package org.jetbrains.kotlin.fir.resolve.calls
 
+import org.jetbrains.kotlin.utils.sure
+
 object FilteringOutOriginalInPresenceOfSmartCastConeCallConflictResolver : ConeCallConflictResolver() {
     override fun chooseMaximallySpecificCandidates(
         candidates: Set<Candidate>,
@@ -14,8 +16,27 @@ object FilteringOutOriginalInPresenceOfSmartCastConeCallConflictResolver : ConeC
         val (originalIfSmartCastPresent, other) = candidates.partition { it.isFromOriginalTypeInPresenceOfSmartCast }
 
         // If we have both successful candidates from smart cast and original, use the former one as they might have more correct return type
+        if (originalIfSmartCastPresent.isNotEmpty() && other.isNotEmpty()) return other.toSet().discriminateByInvokeVariablePriority()
+
+        return candidates.discriminateByInvokeVariablePriority()
+    }
+
+    // See the relevant test at testData/diagnostics/tests/resolve/invoke/kt9517.kt
+    private fun Set<Candidate>.discriminateByInvokeVariablePriority(): Set<Candidate> {
+        if (size <= 1) return this
+
+        // Resulting successful candidates should always belong to the same tower group.
+        // Thus, if one of them is not variable + invoke, it should be applied to others, too.
+        if (first().callInfo.candidateForCommonInvokeReceiver == null) return this
+
+        val (originalIfSmartCastPresent, other) = partition {
+            it.callInfo.candidateForCommonInvokeReceiver.sure {
+                "If one candidate within a group is variable+invoke, other should be the same, but $it found"
+            }.isFromOriginalTypeInPresenceOfSmartCast
+        }
+
         if (originalIfSmartCastPresent.isNotEmpty() && other.isNotEmpty()) return other.toSet()
 
-        return candidates
+        return this
     }
 }

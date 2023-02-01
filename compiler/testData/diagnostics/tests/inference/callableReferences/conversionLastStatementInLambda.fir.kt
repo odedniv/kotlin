e5179b5a2112f47381c0afa90ef959d3368a7df4
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-02-01 11:33:22 +0100

    ~
    ^KT-55936 Fixed

--- compiler/testData/diagnostics/tests/inference/callableReferences/conversionLastStatementInLambda.fir.kt
+++ compiler/testData/diagnostics/tests/inference/callableReferences/conversionLastStatementInLambda.fir.kt
@@ -5,26 +5,26 @@ fun main(b: Boolean) {
     callWithLambda {
         // The only relevant case for KT-55729, Unit conversion should work, but doesn't in K1 1.8.0
         // For K2, it still doesn't work (see KT-55936)
-        ::<!UNRESOLVED_REFERENCE!>test1<!>
+        ::test1
     }
 
     callWithLambda {
         // Unit conversion should work (for K2 see KT-55936)
-        <!ARGUMENT_TYPE_MISMATCH!>if (b) ::test1 else ::test2<!>
+        if (b) ::test1 else ::test2
     }
 
     callWithLambda {
         // That hasn't been working ever in K1 nor K2
-        <!ARGUMENT_TYPE_MISMATCH!>if (b) {
+        if (b) {
             ::test1
         } else {
             ::test2
-        }<!>
+        }
     }
 
     callWithLambda {
         // That hasn't been working ever in K1 nor K2
-        (::<!UNRESOLVED_REFERENCE!>test1<!>)
+        (::test1)
     }
 }
 

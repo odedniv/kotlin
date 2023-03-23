7433e04ecfa61429bafb949d394ab7bba86aa730
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2023-03-23 14:10:08 +0100

    K2: fix field annotation handling around serialization
    Related to KT-57135

--- compiler/fir/fir-serialization/src/org/jetbrains/kotlin/fir/serialization/FirElementSerializer.kt
+++ compiler/fir/fir-serialization/src/org/jetbrains/kotlin/fir/serialization/FirElementSerializer.kt
@@ -339,7 +339,7 @@ class FirElementSerializer private constructor(
         var hasSetter = false
 
         val hasAnnotations = property.nonSourceAnnotations(session).isNotEmpty()
-        // TODO: hasAnnotations(descriptor) || hasAnnotations(descriptor.backingField) || hasAnnotations(descriptor.delegateField)
+                || property.backingField?.nonSourceAnnotations(session)?.isNotEmpty() == true
 
         val modality = property.modality!!
         val defaultAccessorFlags = Flags.getAccessorFlags(

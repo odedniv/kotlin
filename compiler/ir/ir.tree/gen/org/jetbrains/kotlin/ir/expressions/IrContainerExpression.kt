c5a681c29023e481ef363fa3311f15a2205f2715
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-03-28 18:13:05 +0200

    replace back SmartList width ArrayList inside IrContainerExpression

--- compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/expressions/IrContainerExpression.kt
+++ compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/expressions/IrContainerExpression.kt
@@ -12,7 +12,6 @@ import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.util.transformInPlace
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformer
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitor
-import org.jetbrains.kotlin.utils.SmartList
 
 /**
  * A non-leaf IR tree element.
@@ -22,7 +21,7 @@ import org.jetbrains.kotlin.utils.SmartList
 abstract class IrContainerExpression : IrExpression(), IrStatementContainer {
     abstract var origin: IrStatementOrigin?
 
-    override val statements: MutableList<IrStatement> = SmartList()
+    override val statements: MutableList<IrStatement> = ArrayList(2)
 
     override fun <D> acceptChildren(visitor: IrElementVisitor<Unit, D>, data: D) {
         statements.forEach { it.accept(visitor, data) }

f91184095949a4eb338db7e878d29997986aff61
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-04-06 19:34:57 +0200

    fixup! [LL FIR] add ability to suppress contract violation exception
    Reuse one exception class between tests and production

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/lazy/resolve/LLFirLazyResolveContractChecker.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/lazy/resolve/LLFirLazyResolveContractChecker.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir.lazy.resolve
 
 import com.intellij.openapi.diagnostic.Logger
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
+import org.jetbrains.kotlin.fir.symbols.FirLazyResolveContractViolationException
 
 internal class LLFirLazyResolveContractChecker {
     private val currentTransformerPhase = ThreadLocal.withInitial<FirResolvePhase?> { null }
@@ -27,20 +28,12 @@ internal class LLFirLazyResolveContractChecker {
         val currentPhase = currentTransformerPhase.get() ?: return
 
         if (requestedPhase >= currentPhase) {
-            logOrThrowException(
-                """`lazyResolveToPhase($requestedPhase)` cannot be called from a transformer with a phase $currentPhase.
-                    `lazyResolveToPhase` can be called only from a transformer with a phase which is strictly greater than a requested phase;
-                     i.e., `lazyResolveToPhase(A)` may be only called from a lazy transformer with a phase B, where A < B. This is a contract of lazy resolve""".trimIndent()
-            )
-        }
-    }
-
-    private fun logOrThrowException(text: String) {
-        val exception = FirLazyResolveContractViolationException(text)
-        if (System.getProperty("kotlin.suppress.lazy.resolve.contract.violation") != null) {
-            LoggerHolder.LOG.warn(exception)
-        } else {
-            throw exception
+            val exception = FirLazyResolveContractViolationException(currentPhase = currentPhase, requestedPhase = requestedPhase)
+            if (System.getProperty("kotlin.suppress.lazy.resolve.contract.violation") != null) {
+                LoggerHolder.LOG.warn(exception)
+            } else {
+                throw exception
+            }
         }
     }
 
@@ -48,5 +41,3 @@ internal class LLFirLazyResolveContractChecker {
         val LOG = Logger.getInstance(LLFirLazyResolveContractChecker::class.java)
     }
 }
-
-private class FirLazyResolveContractViolationException(message: String) : IllegalStateException(message)

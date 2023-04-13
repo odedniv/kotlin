d7e3aba4f19419c47120489d19424e5abb2e7536
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2023-04-13 10:04:21 +0200

    migrated NativeEmbeddableCompilerJarIT to junit 5 and gradle TestKit
    #KT-51553

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/NativeEmbeddableCompilerJarIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/NativeEmbeddableCompilerJarIT.kt
@@ -1,21 +1,20 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.gradle.native
 
-import org.jetbrains.kotlin.gradle.BaseGradleIT
-import org.jetbrains.kotlin.gradle.GradleVersionRequired
-import org.jetbrains.kotlin.gradle.native.GeneralNativeIT.Companion.withNativeCompilerClasspath
-import org.junit.Test
+import org.gradle.api.logging.LogLevel
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.testbase.*
+import org.junit.jupiter.api.DisplayName
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-class NativeEmbeddableCompilerJarIT : BaseGradleIT() {
-
-    override val defaultGradleVersion: GradleVersionRequired
-        get() = GradleVersionRequired.FOR_MPP_SUPPORT
+@DisplayName("Tests for K/N builds with embeddable compiler jar")
+@NativeGradlePluginTests
+internal class NativeEmbeddableCompilerJarIT : KGPBaseTest() {
 
     private fun String.isRegularJar() = this.endsWith("/kotlin-native.jar")
     private fun String.isEmbeddableJar() = this.endsWith("/kotlin-native-compiler-embeddable.jar")
@@ -23,59 +22,80 @@ class NativeEmbeddableCompilerJarIT : BaseGradleIT() {
     private fun List<String>.includesRegularJar() = any { it.isRegularJar() }
     private fun List<String>.includesEmbeddableJar() = any { it.isEmbeddableJar() }
 
-    @Test
-    fun testDefault() = with(transformNativeTestProjectWithPluginDsl("executables", directoryPrefix = "native-binaries")) {
-        build(":linkDebugExecutableHost") {
-            assertSuccessful()
-            withNativeCompilerClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
-                assertFalse(it.includesRegularJar())
-                assertTrue(it.includesEmbeddableJar())
+    private val String.withPrefix get() = "native-binaries/$this"
+
+    @DisplayName("K/N with default config shouldn't contain kotlin-native.jar and should contain kotlin-native-compiler-embeddable.jar")
+    @GradleTest
+    fun shouldNotUseRegularJarInDefaultConfig(gradleVersion: GradleVersion) {
+        nativeProject("executables".withPrefix, gradleVersion) {
+            build(":linkDebugExecutableHost", buildOptions = buildOptions.copy(logLevel = LogLevel.DEBUG)) {
+                assertTasksClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
+                    assertFalse(it.includesRegularJar())
+                    assertTrue(it.includesEmbeddableJar())
+                }
             }
         }
     }
 
-    @Test
-    fun testEmbeddableJarFalse() = with(transformNativeTestProjectWithPluginDsl("executables", directoryPrefix = "native-binaries")) {
-        build(":linkDebugExecutableHost", "-Pkotlin.native.useEmbeddableCompilerJar=false") {
-            assertSuccessful()
-            withNativeCompilerClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
-                assertTrue(it.includesRegularJar())
-                assertFalse(it.includesEmbeddableJar())
+    @DisplayName("K/N with embeddable compiler flag turned off config should contain kotlin-native.jar and shouldn't contain kotlin-native-compiler-embeddable.jar")
+    @GradleTest
+    fun shouldUseRegularJarWithoutUseEmbeddableCompilerJar(gradleVersion: GradleVersion) {
+        nativeProject("executables".withPrefix, gradleVersion) {
+            build(
+                ":linkDebugExecutableHost",
+                "-Pkotlin.native.useEmbeddableCompilerJar=false",
+                buildOptions = buildOptions.copy(logLevel = LogLevel.DEBUG)
+            ) {
+                assertTasksClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
+                    assertTrue(it.includesRegularJar())
+                    assertFalse(it.includesEmbeddableJar())
+                }
             }
         }
     }
 
-    @Test
-    fun testEmbeddableJarTrue() = with(transformNativeTestProjectWithPluginDsl("executables", directoryPrefix = "native-binaries")) {
-        build(":linkDebugExecutableHost", "-Pkotlin.native.useEmbeddableCompilerJar=true") {
-            assertSuccessful()
-            withNativeCompilerClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
-                assertFalse(it.includesRegularJar())
-                assertTrue(it.includesEmbeddableJar())
+    @DisplayName("K/N with embeddable compiler flag turned off config shouldn't contain kotlin-native.jar and should contain kotlin-native-compiler-embeddable.jar")
+    @GradleTest
+    fun shouldUseRegularJarWithUseEmbeddableCompilerJar(gradleVersion: GradleVersion) {
+        nativeProject("executables".withPrefix, gradleVersion) {
+            build(
+                ":linkDebugExecutableHost",
+                "-Pkotlin.native.useEmbeddableCompilerJar=true",
+                buildOptions = buildOptions.copy(logLevel = LogLevel.DEBUG)
+            ) {
+                assertTasksClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
+                    assertFalse(it.includesRegularJar())
+                    assertTrue(it.includesEmbeddableJar())
+                }
             }
         }
     }
 
-    @Test
-    fun testSwitch() = with(transformNativeTestProjectWithPluginDsl("executables", directoryPrefix = "native-binaries")) {
-        build(":linkDebugExecutableHost") {
-            assertSuccessful()
-            withNativeCompilerClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
-                assertFalse(it.includesRegularJar())
-                assertTrue(it.includesEmbeddableJar())
+    @DisplayName("K/N with embeddable compiler flag turned off config shouldn't contain kotlin-native.jar and should contain kotlin-native-compiler-embeddable.jar")
+    @GradleTest
+    fun testSwitch(gradleVersion: GradleVersion) {
+        nativeProject("executables".withPrefix, gradleVersion) {
+            build(":linkDebugExecutableHost", buildOptions = buildOptions.copy(logLevel = LogLevel.DEBUG)) {
+                assertTasksClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
+                    assertFalse(it.includesRegularJar())
+                    assertTrue(it.includesEmbeddableJar())
+                }
             }
-        }
 
-        build(":linkDebugExecutableHost") {
-            assertTasksUpToDate(":linkDebugExecutableHost", ":compileKotlinHost")
-        }
+            build(":linkDebugExecutableHost") {
+                assertTasksUpToDate(":linkDebugExecutableHost", ":compileKotlinHost")
+            }
 
-        build(":linkDebugExecutableHost", "-Pkotlin.native.useEmbeddableCompilerJar=false") {
-            assertSuccessful()
-            assertTasksExecuted(":linkDebugExecutableHost", ":compileKotlinHost")
-            withNativeCompilerClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
-                assertTrue(it.includesRegularJar())
-                assertFalse(it.includesEmbeddableJar())
+            build(
+                ":linkDebugExecutableHost",
+                "-Pkotlin.native.useEmbeddableCompilerJar=false",
+                buildOptions = buildOptions.copy(logLevel = LogLevel.DEBUG)
+            ) {
+                assertTasksExecuted(":linkDebugExecutableHost", ":compileKotlinHost")
+                assertTasksClasspath(":linkDebugExecutableHost", ":compileKotlinHost") {
+                    assertTrue(it.includesRegularJar())
+                    assertFalse(it.includesEmbeddableJar())
+                }
             }
         }
     }

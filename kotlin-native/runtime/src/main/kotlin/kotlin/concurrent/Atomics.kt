8a2277849f4dfac13749dea1c3fdf128bcb9730e
mvicsokolova <maria.sokolova@jetbrains.com>
2023-04-19 10:29:27 +0200

    Removed internal swap fun from kotlin.concurrent.AtomicReference

--- kotlin-native/runtime/src/main/kotlin/kotlin/concurrent/Atomics.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/concurrent/Atomics.kt
@@ -195,7 +195,17 @@ public class AtomicReference<T> {
     /**
      * Atomically sets the value to the given [new value][newValue] and returns the old value.
      */
-    public fun getAndSet(newValue: T): T = swap(newValue)
+    public fun getAndSet(newValue: T): T {
+        while (true) {
+            val old = value
+            if (old === newValue) {
+                return old
+            }
+            if (compareAndSet(old, newValue)) {
+                return old
+            }
+        }
+    }
 
     /**
      * Atomically sets the value to the given [new value][newValue] if the current value equals the [expected value][expected],
@@ -228,18 +238,6 @@ public class AtomicReference<T> {
      */
     public override fun toString(): String =
             "${debugString(this)} -> ${debugString(value)}"
-
-    internal fun swap(newValue: T): T {
-        while (true) {
-            val old = value
-            if (old === newValue) {
-                return old
-            }
-            if (compareAndSet(old, newValue)) {
-                return old
-            }
-        }
-    }
 
     // Implementation details.
     @GCUnsafeCall("Kotlin_AtomicReference_set")
